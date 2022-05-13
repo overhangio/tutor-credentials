f1a796819fe7ad238be3989cefe99c2e5b84b986
lpm0073 <lpm0073@gmail.com>
2022-05-13 18:57:16 -0500

    copy tutor-ecommerce pattern to remove local logging

--- tutorcredentials/templates/credentials/apps/credentials/settings/partials/common.py
+++ tutorcredentials/templates/credentials/apps/credentials/settings/partials/common.py
@@ -1,8 +1,10 @@
 from credentials.settings.utils import get_logger_config
 
-# Get rid of local logger
+# reconfigure logging and Get rid of local logger
 LOGGING = get_logger_config(debug=False, dev_env=True, local_loglevel="INFO")
-del LOGGING["handlers"]["local"]
+LOGGING["handlers"].pop("local")
+for logger in LOGGING["loggers"].values():
+    logger["handlers"].remove("local")
 
 
 {{ patch("credentials-settings-common") }}

63a77aac38fd569e083521531ff16a96569d1b4a
lpm0073 <lpm0073@gmail.com>
2022-05-13 13:11:34 -0500

    CORS and CSRF settings do not need to be modified

--- tutorcredentials/templates/credentials/apps/credentials/settings/development.py
+++ tutorcredentials/templates/credentials/apps/credentials/settings/development.py
@@ -2,10 +2,6 @@ from ..devstack import *
 
 {% include "credentials/apps/credentials/settings/partials/common.py" %}
 
-CORS_ORIGIN_WHITELIST = list(CORS_ORIGIN_WHITELIST) + [
-    "http://{{ MFE_HOST }}:{{ CREDENTIALS_MFE_APP['port'] }}",
-]
-CSRF_TRUSTED_ORIGINS = ["{{ MFE_HOST }}:{{ CREDENTIALS_MFE_APP['port'] }}"]
 
 SOCIAL_AUTH_EDX_OAUTH2_PUBLIC_URL_ROOT = "http://{{ LMS_HOST }}:8000"
 

66ef23c4800bd7fd76625cae35644689209c445b
lpm0073 <lpm0073@gmail.com>
2022-05-14 13:06:49 -0500

    configure editor defaults

--- linter.sh
+++ linter.sh
@@ -5,7 +5,7 @@
 #
 # date:         mar-2022
 #
-# usage:        Runs terraform fmt -recursive
+# usage:        run all pre-commit checks.
 #------------------------------------------------------------------------------
 
 pre-commit run --all-files
